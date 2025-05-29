60b46d409049cf43f9b173dec6fb5c0f4664cf14
DiogoWolfie <diogopl1@al.insper.edu.br>
2025-05-28 23:00:01 -0300

    refactor: update memory initialization in Block and fix Output constructor

--- ast/nodes/blocknode.py
+++ ast/nodes/blocknode.py
@@ -1,11 +1,12 @@
 from ast.node import Node
+from collections import defaultdict
 
 class Block(Node):
     def __init__(self, children):
         super().__init__(None, children)
 
     def Evaluate(self, memory=None, pointer=None):
-        memory = [0] * 30000 if memory is None else memory
+        memory = defaultdict(int) if memory is None else memory
         pointer = [0] if pointer is None else pointer
 
         for child in self.children:
--- ast/nodes/outputnode.py
+++ ast/nodes/outputnode.py
@@ -2,7 +2,7 @@ from ast.node import Node
 
 class Output(Node):
     def __init__(self):
-        super().__init__(None)
+        super().__init__(None, [])
 
     def Evaluate(self, memory, pointer):
-        print(chr(memory[pointer[0]]), end='') 
+        print(chr(memory[pointer[0]]), end='')
